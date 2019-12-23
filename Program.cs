ee42726485b4070fa0ff137aefb97abe579df6fa
Matt Obert on Windows <matt@hotwebmatter.com>
2019-12-23 10:36:38 -0500

    Declare class-level static variables

--- Program.cs
+++ Program.cs
@@ -10,6 +10,15 @@ namespace take_home_methods
                      FEDERAL_TAX_RATE = .18,
                      RETIREMENT_CONTRIB = .15,
                      SOCIAL_SECURITY = .09;
+        // declare class-level static variables
+        static string employeeName = string.Empty;
+        static double employeeSales,
+                      employeeCommission,
+                      federalTax,
+                      retirementContrib,
+                      socialSecurity,
+                      totalDeductions,
+                      takeHomePay;
         static void Main(string[] args)
         {
             WriteLine("Hello World!");
