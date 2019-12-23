3ebe5647fbc109cc07ef58aa42ef9b149037cbca
Matt Obert on Windows <matt@hotwebmatter.com>
2019-12-23 10:29:14 -0500

    Define class-level constants

--- Program.cs
+++ Program.cs
@@ -5,6 +5,11 @@ namespace take_home_methods
 {
     class Program
     {
+        // declare class-level constants
+        const double COMMISSION = .07,
+                     FEDERAL_TAX_RATE = .18,
+                     RETIREMENT_CONTRIB = .15,
+                     SOCIAL_SECURITY = .09;
         static void Main(string[] args)
         {
             WriteLine("Hello World!");
